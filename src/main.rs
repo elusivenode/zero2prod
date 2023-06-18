a0303d49282d089c360740ce4647a0a22329b1de
Hamish MacDonald <hamish.macdonald@Hamishs-MBP.modem>
2023-06-18 12:30:15 +1000

    subscriptions route write to db

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
-use zero2prod::run;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     // We retrieve the port assigned to us by the OS
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
 
     let port = listener.local_addr().unwrap().port();
     println!("Running at http://127.0.0.1:{}", port);
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
 
 // up to page 81
